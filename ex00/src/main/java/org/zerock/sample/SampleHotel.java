af04e0868fd0c6ebc1cdad7f88283b931c102526
seoki <jeongseokdong@gmail.com>
2022-08-18 10:46:50 +0900

    ex00

---
+++ ex00/src/main/java/org/zerock/sample/SampleHotel.java
@@ -0,0 +1,22 @@
+package org.zerock.sample;
+
+
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Component
+@ToString
+@Getter
+@RequiredArgsConstructor
+public class SampleHotel {
+	
+	@NonNull
+	private Chef chef;
+
+
+}
